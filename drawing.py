97b18c21b8b993cbd3d9b4907469c0cf49285f42
Luis Banuelos <luis.banuelos.chacon@hp.com>
2017-06-17 18:30:00 -0700

    Refactorsote

--- drawing.py
+++ drawing.py
@@ -1,27 +1,67 @@
-from array import array
+from copy import deepcopy
 import math
+import time
 import sys
 
 
 class Painter(object):
 
-    def __init__(self, width, height):
+    cmap = {
+        # key definition
+        # (up, down, left)
+
+        # SINGLES
+        # straights
+        (0, 0, 1, 1): '─', (1, 1, 0, 0): '│',
+        # corners
+        (0, 1, 0, 1): '┌', (0, 1, 1, 0): '┐', (1, 0, 0, 1): '└', (1, 0, 1, 0): '┘',
+        # intersections
+        (1, 1, 0, 1): '├', (1, 1, 1, 0): '┤', (0, 1, 1, 1): '┬', (1, 0, 1, 1): '┴', (1, 1, 1, 1): '┼',
+        # ends
+        (1, 0, 0, 0): '┴', (0, 1, 0, 0): '┬', (0, 0, 1, 0): '┤', (0, 0, 0, 1): '├',
+
+        # DOUBLES
+        # straights
+        (0, 0, 2, 2): '═', (2, 2, 0, 0): '║',
+        # corners
+        (0, 2, 0, 2): '╔', (0, 2, 2, 0): '╗', (2, 0, 0, 2): '╚', (2, 0, 2, 0): '╝',
+        # intersections
+        (2, 2, 0, 2): '╠', (2, 2, 2, 0): '╣', (0, 2, 2, 2): '╦', (2, 0, 2, 2): '╩', (2, 2, 2, 2): '╬',
+        # ends
+        (2, 0, 0, 0): '╨', (0, 2, 0, 0): '╥', (0, 0, 2, 0): '╡', (0, 0, 0, 2): '╞',
+
+        # INTERSECTION TRANSITION
+        (0, 1, 0, 2): '╒', (0, 2, 0, 1): '╓', (0, 1, 2, 0): '╕', (0, 2, 1, 0): '╖', (1, 0, 0, 2): '╘',
+        (2, 0, 0, 1): '╙', (1, 0, 2, 0): '╛', (2, 0, 1, 0): '╜', (1, 1, 0, 2): '╞', (2, 2, 0, 1): '╟',
+        (1, 1, 2, 0): '╡', (2, 2, 1, 0): '╢', (0, 1, 2, 2): '╤', (0, 2, 1, 1): '╥', (1, 0, 2, 2): '╧',
+        (2, 0, 1, 1): '╨', (1, 1, 2, 2): '╪', (2, 2, 1, 1): '╫',
+    }
+
+    def __init__(self, width, height, stream=sys.stdout):
         self._width = width
         self._height = height
+        self._stream = stream
         self._buffer = [[None for x in range(self._width)] for y in range(self._height)]
         self._area = [0, 0, width, height]
         self._area_stack = []
 
+    ##
+    # Drawing Area
+    ##
+
     @property
     def width(self):
+        '''Effective drawing width.'''
         return self._area[2]
 
     @property
     def height(self):
+        '''Effective drawing height.'''
         return self._area[3]
 
     @property
     def size(self):
+        '''Effective drawing size.'''
         return [self._area[2], self._area[3]]
 
     def pushArea(self, x, y, w, h):
@@ -34,9 +74,9 @@ class Painter(object):
     def resetArea(self):
         self._area = [0, 0, self._width, self._height]
 
-    def draw(self, x, y, v):
-        x0, y0, w, h = self._area
-        self._buffer[y0 + y][x0 + x] = v
+    ##
+    # Painting Methods
+    ##
 
     def clear(self):
         x0, y0, w, h = self._area
@@ -44,134 +84,203 @@ class Painter(object):
             for x in range(x0, x0+w):
                 self._buffer[y][x] = None
 
+    def set(self, x, y, v):
+        x0, y0, w, h = self._area
+        self._buffer[y0 + y][x0 + x] = v
+
+    def get(self, x, y):
+        x0, y0, w, h = self._area
+        if (0 <= x < w) and (0 <= y < h):
+            return self._buffer[y0 + y][x0 + x]
+
     def drawText(self, x, y, text):
         for i, c in enumerate(text):
-            self.draw(x+i, y, c)
+            self.set(x+i, y, c)
 
     def drawHorizontal(self, x, y, l, v):
         for ix in range(x + l):
-            self.draw(x+ix, y, v)
+            self.set(x+ix, y, v)
 
     def drawVertical(self, x, y, l, v):
         for iy in range(y + l):
-            self.draw(x, y+iy, v)
+            self.set(x, y+iy, v)
 
     def drawRectangle(self, x, y, w, h, v):
         '''Draw rectangle with value.'''
         for ix in range(x + w):
-            self.draw(ix, y, v)
-            self.draw(ix, y+h-1, v)
+            self.set(ix, y, v)
+            self.set(ix, y+h-1, v)
 
         for iy in range(y + h):
-            self.draw(x, iy, v)
-            self.draw(x+w-1, iy, v)
+            self.set(x, iy, v)
+            self.set(x+w-1, iy, v)
 
     def insert(self, ox, oy, m):
         '''Insert m into buffer at (x, y)'''
         for y, rows in enumerate(m):
             for x, data in enumerate(rows):
-                self.draw(x+ox, y+oy, data)
+                self.set(x+ox, y+oy, data)
 
-    def print(self):
-        for row in self._buffer:
+    ##
+    # Post Processing
+    ##
+
+    def boxify(self):
+        x0, y0, w, h = self._area
+        out = deepcopy(self._buffer)
+        for y in range(h):
+            for x in range(w):
+                # if this is a marker
+                if self._getMarker(x, y):
+                    key = []
+                    count = 0
+                    points = ((x, y-1), (x, y+1), (x-1, y), (x+1, y))
+                    for point in points:
+                        marker = self._getMarker(*point)
+                        count += marker != 0
+                        key.append(marker)
+
+                    # key error means symbol is adjacent to intersection
+                    try:
+                        out[y0+y][x0+x] = self.cmap[tuple(key)]
+                    except KeyError:
+                        pass
+
+                    # if intersection, insert adjacent symbols
+                    if count > 2:
+                        if key[0]:
+                            out[y0+y-1][x0+x] = self.cmap[(key[0], key[0], 0, 0)]
+                        if key[1]:
+                            out[y0+y+1][x0+x] = self.cmap[(key[1], key[1], 0, 0)]
+                        if key[2]:
+                            out[y0+y][x0+x-1] = self.cmap[(0, 0, key[2], key[2])]
+                        if key[3]:
+                            out[y0+y][x0+x+1] = self.cmap[(0, 0, key[3], key[3])]
+
+        self._buffer = out
+
+    def _getMarker(self, x, y):
+        marker = self.get(x, y)
+        if isinstance(marker, int):
+            return marker
+        return 0
+
+    ##
+    # Printing on Stream
+    ##
+
+    def print(self, overdraw=False):
+        if overdraw:
+            self._return()
+        for i, row in enumerate(self._buffer):
             for c in row:
                 if c is None:
                     sys.stdout.write(' ')
                 else:
-                    sys.stdout.write(c)
-            sys.stdout.write('\n')
+                    sys.stdout.write(str(c))
+            if i < len(self._buffer):
+                sys.stdout.write('\n')
+
+    def _return(self):
+        self._stream.write('\033[F' * len(self._buffer))
 
 
 class Item(object):
 
     def render(self, p):
-        p.drawRectangle(0, 0, p.width, p.height, '.')
-        p.drawText(1, 1, 'This is me.')
+        p.drawRectangle(0, 0, p.width, p.height, ' ')
 
 
 class Layout(object):
 
-    def __init__(self, direction='v', border=True, margin=0, marker='1'):
-        self._direction = direction
-        self._margin = margin
-        self._border = border
-        self._marker = marker
-        self._items = []
+    def __init__(self, direction='v', border='2', divider='1'):
+        self._direction = direction     # layout direction
+        self._divider = divider         # divider marker
+        self._border = border           # border marker
+        self._items = []                # item list
 
     def addItem(self, item, weight=1):
         self._items.append((weight, item))
 
     def render(self, p):
-        if self._direction == 'h':
-            dim_a = p.width
-            dim_b = p.height
-        else:
-            dim_b = p.width
-            dim_a = p.height
-
-        # calculate dividers
-        divs = len(self._items) - 1
-        div_length = (self._margin * 2) + 1
-
-        # add border
+        # draw border and reduce area
         if self._border:
-            p.drawRectangle(0, 0, p.width, p.height, self._marker)
-            border = 1
-        else:
-            border = 0
+            p.drawRectangle(0, 0, p.width, p.height, self._border)
+            p.pushArea(1, 1, p.width-2, p.height-2)
 
-        # indentify size distribution
-        fixed_length = 0
-        total_weight = 0.0
+        if self._direction == 'v':
+            # get distribution
+            dist = self._lengthDistribution(p.height)
+
+            # draw dividers and items
+            y = 0
+            for i, pair in enumerate(self._items):
+                w, item = pair
+
+                if (i > 0) and self._divider:
+                    p.drawHorizontal(0, y, p.width, self._divider)
+                    y += 1
+
+                p.pushArea(0, y, p.width, dist[i])
+                item.render(p)
+                p.popArea()
+
+                y += dist[i]
+
+        else:
+            # get distribution
+            dist = self._lengthDistribution(p.width)
+
+            # draw dividers and items
+            x = 0
+            for i, pair in enumerate(self._items):
+                w, item = pair
+
+                if (i > 0) and self._divider:
+                    p.drawVertical(x, 0, p.height, self._divider)
+                    x += 1
+
+                p.pushArea(x, 0, dist[i], p.height)
+                item.render(p)
+                p.popArea()
+
+                x += dist[i]
+
+        # pop to border and boxify
+        if self._border:
+            p.popArea()
+            p.boxify()
+
+    def _lengthDistribution(self, length):
+        '''Distributes length across items.'''
+        # count dividers
+        dividers = (len(self._items) - 1) * (self._divider is not None)
+        # total fixed length
+        fixed_length = sum([w for w, i in self._items if w > 1.0])
+        # total weight
+        total_weight = sum([w for w, i in self._items if w <= 1.0])
+        # total distributable length is drawable area - dividers - fixed length
+        total_length = length - dividers - fixed_length
+        # generate length list
+        lengths = []
         for weight, item in self._items:
             if weight > 1.0:
-                fixed_length += int(weight)
+                lengths.append(int(weight))
             else:
-                total_weight += weight
-
-        # total length to be distributed to content
-        total_length = dim_a - (border * 2) - (self._margin * 2) - (div_length * divs) - fixed_length
-        # container draw length
-        draw_length = dim_b - (border * 2) - (self._margin * 2)
-
-        # draw items
-        i = self._margin + border
-        for num, pair in enumerate(self._items):
-            weight, item = pair
-            if weight > 1.0:
-                length = weight
-            else:
-                length = math.floor((weight / total_weight) * total_length)
-
-            # draw
-            if self._direction == 'h':
-                if num != 0:
-                    p.drawVertical(i-self._margin-1, 0, dim_b, self._marker)
-                p.pushArea(i, self._margin + border, length, draw_length)
-            else:
-                if num != 0:
-                    p.drawHorizontal(0, i-self._margin-1, dim_b, self._marker)
-                p.pushArea(self._margin + border, i, draw_length, length)
-            item.render(p)
-            p.popArea()
-
-            i += div_length + length
-
-
-class Table(Item):
-
-    def __init__(self, header, data):
-        self.header = header
-        self.data = data
-
-
+                lengths.append(int((weight / total_weight) * total_length))
+        # compensate for fractional error
+        if sum(lengths) < total_length:
+            lengths[-1] += 1
+        # return list
+        return lengths
 
 
 if __name__ == '__main__':
-    p = Painter(81, 21)
-    panel0 = Layout(direction='v', margin=0, marker='1')
-    panel1 = Layout(direction='h', margin=0, border=False, marker='2')
-    panel2 = Layout(direction='v', margin=0, border=False, marker='3')
+    p = Painter(60, 21)
+    # Layout(direction, border, divider)
+    panel0 = Layout('v', 2, 1)
+    panel1 = Layout('h', 0, 1)
+    panel2 = Layout('v', 2, 1)
     panel0.addItem(Item())
     panel0.addItem(panel1)
     panel1.addItem(Item())
@@ -179,4 +288,4 @@ if __name__ == '__main__':
     panel2.addItem(Item())
     panel2.addItem(Item())
     panel0.render(p)
-    p.print()
+    p.print(False)
